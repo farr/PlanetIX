c2c6709ad550d75c104838c616509e42e8e6c9ce
Will Meierjurgen Farr <wmfarr@gmail.com>
2016-02-24 23:19:44 +0000

    Added Figure 8 code.

---
+++ figure_8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from pylab import *
+
+import gzip
+import os
+import pickle
+import rebound as re
+import utils as u
+
+if __name__ == '__main__':
+    loadfile = 'simulation.save'
+    if os.path.exists(loadfile):
+        sim = re.Simulation.from_file(loadfile)
+    else:
+        sim = u.outer_solar_system()
+        sim = u.add_canonical_inclined_perturber(sim)
+        sim = u.add_inclined_aligned_testmass_disk(sim, 400)
+
+    for t in linspace(0, 2*pi*1e9, 1000):
+        sim.integrate(t, exact_finish_time=0)
+
+        with gzip.open('orbits.pkl.gz', 'a') as out:
+            pickle.dump((sim.t, sim.calculate_orbits()), out)
+
+        tfile = 'simulation.save.temp'
+        sfile = 'simulation.save'
+        sim.save(tfile)
+        os.rename(tfile, sfile)
+
+        print 'Evolved to ', sim.t
--- utils.py
+++ utils.py
@@ -17,6 +17,10 @@ def add_canonical_planar_perturber(sim):
     sim.add(m=3e-5, a=700, e=0.6, pomega=2*pi*rand(), l=2*pi*rand())
     return sim
 
+def add_canonical_inclined_perturber(sim):
+    sim.add(m=3e-5, a=700, e=0.6, inc=30*pi/180.0, omega=150*pi/180.0, Omega=2*pi*rand(), l=2*pi*rand())
+    return sim
+
 def add_planar_testmass_disk(sim, Npart):
     N_active = sim.N
 
@@ -27,6 +31,23 @@ def add_planar_testmass_disk(sim, Npart):
         pomega = 2*pi*rand()
 
         sim.add(m=0, a=a, e=e, pomega=pomega, l=2*pi*rand())
+
+    sim.N_active = N_active
+
+    return sim
+
+def add_inclined_aligned_testmass_disk(sim, Npart):
+    N_active = sim.N
+
+    porb = sim.calculate_orbits()[-1]
+        
+    for i in range(Npart):
+        a = 150 + 400*rand()
+        q = 30 + 20*rand()
+        e = 1 - q/a
+        inc = abs(15*pi/180.0*randn())
+
+        sim.add(m=0, a=a, e=e, inc=inc, Omega=porb.Omega, pomega=porb.pomega + pi, l=2*pi*rand())
 
     sim.N_active = N_active
 
