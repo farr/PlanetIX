671450f0b1efa9a99919dd417d88e1ae7fc052f4
Will Meierjurgen Farr <wmfarr@gmail.com>
2016-02-24 22:54:06 +0000

    Added figure_5.py

--- utils.py
+++ utils.py
@@ -16,3 +16,18 @@ def outer_solar_system():
 def add_canonical_planar_perturber(sim):
     sim.add(m=3e-5, a=700, e=0.6, pomega=2*pi*rand(), l=2*pi*rand())
     return sim
+
+def add_planar_testmass_disk(sim, Npart):
+    N_active = sim.N
+
+    for i in range(Npart):
+        a = 50 + 500*rand()
+        q = 30 + 20*rand()
+        e = 1 - q/a
+        pomega = 2*pi*rand()
+
+        sim.add(m=0, a=a, e=e, pomega=pomega, l=2*pi*rand())
+
+    sim.N_active = N_active
+
+    return sim
