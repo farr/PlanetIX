bd7e82ea393031de47d461dcd4b9f44586cb1d0f
Will Meierjurgen Farr <wmfarr@gmail.com>
2016-02-27 01:12:00 +0000

    Only save state after loading on the first new timestep and subsequently

--- figure_4.py
+++ figure_4.py
@@ -11,8 +11,12 @@ import utils as u
 if __name__ == '__main__':
     loadpath = 'simulation.save'
     if os.path.exists(loadpath):
+        loaded = True
+        start_t = sim.t
         sim = re.Simulation.from_file(loadpath)
-    else:       
+    else:
+        loaded = False
+        start_t = 0
         sim = u.outer_solar_system()
         sim = u.add_canonical_planar_perturber(sim)
         for e in linspace(0, 1, 21)[:-1]:
@@ -23,12 +27,13 @@ if __name__ == '__main__':
     for t in linspace(sim.t, sim.t+2*pi*1e9, 1000):
         sim.integrate(t, exact_finish_time=0)
 
-        with gzip.open('orbits.pkl.gz', 'a') as out:
-            pickle.dump((sim.t, sim.calculate_orbits()), out)
+        if not (loaded and sim.t == start_t):
+            with gzip.open('orbits.pkl.gz', 'a') as out:
+                pickle.dump((sim.t, sim.calculate_orbits()), out)
 
-        tfile = 'simulation.save.temp'
-        ofile = 'simulation.save'
-        sim.save(tfile)
-        os.rename(tfile, ofile)
+            tfile = 'simulation.save.temp'
+            ofile = 'simulation.save'
+            sim.save(tfile)
+            os.rename(tfile, ofile)
 
 	print 'Evolved to ', sim.t
--- figure_8.py
+++ figure_8.py
@@ -12,7 +12,11 @@ if __name__ == '__main__':
     loadfile = 'simulation.save'
     if os.path.exists(loadfile):
         sim = re.Simulation.from_file(loadfile)
+        loaded = True
+        start_t = sim.t
     else:
+        loaded = False
+        start_t = 0
         sim = u.outer_solar_system()
         sim = u.add_canonical_inclined_perturber(sim)
         sim = u.add_inclined_aligned_testmass_disk(sim, 400)
@@ -20,12 +24,13 @@ if __name__ == '__main__':
     for t in linspace(sim.t, sim.t+2*pi*1e9, 1000):
         sim.integrate(t, exact_finish_time=0)
 
-        with gzip.open('orbits.pkl.gz', 'a') as out:
-            pickle.dump((sim.t, sim.calculate_orbits()), out)
+        if not (loaded and start_t == sim.t):
+            with gzip.open('orbits.pkl.gz', 'a') as out:
+                pickle.dump((sim.t, sim.calculate_orbits()), out)
 
-        tfile = 'simulation.save.temp'
-        sfile = 'simulation.save'
-        sim.save(tfile)
-        os.rename(tfile, sfile)
+            tfile = 'simulation.save.temp'
+            sfile = 'simulation.save'
+            sim.save(tfile)
+            os.rename(tfile, sfile)
 
         print 'Evolved to ', sim.t
